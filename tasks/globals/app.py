80612b931a0d2f15eace7c46c7b344a1295f8691
Jakub Nowak <jakub.jakub.nowak@gmail.com>
2021-08-26 13:03:20 +0200

    [globals]

---
+++ tasks/globals/app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template_string
+
+app = Flask(__name__)
+
+with open('flag.txt') as f:
+    FLAG = f.read()
+
+@app.route('/')
+def home():
+    return render_template_string(
+    '''
+        <form id=form action='#'>
+            <label for=name_>Gib your name</label>
+            <input id=name_ />
+            <input type=submit style=display:none />
+        </form>
+        <script>
+            function submit(event) {
+                event.preventDefault();
+                location.assign('/' + name_.value);
+            }
+            form.onsubmit = submit;
+        </script>
+    ''')
+
+@app.route('/<name>')
+def hello(name):
+    return render_template_string('Hello ' + name + '!')
+
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 8000, debug = False)
